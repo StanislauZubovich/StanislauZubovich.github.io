ecef6edbf38ea3cde4c9620aa251d8bf2f5e7d29
Stanislau Zubovich <Stanislau_Zubovich@epam.com>
2015-12-10 14:08:43 +0300

    last update wcag for wsd

--- WCAG_WSD/index.js
+++ WCAG_WSD/index.js
@@ -14,4 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 		return false;
 	};
+	var v1 = document.getElementById('point');
+	console.log(v);
+	v1.addEventListener('play', function () {
+		v1.play();
+	}, false);
+	v1.onclick = function () {
+		if (v1.paused) {
+			v1.play();
+		} else {
+			v1.pause();
+		}
+		return false;
+	};
 }, false);
