d04e49293095d5f9a4e7d79bc17a75bc8e465ddc
Ernest Yurkoits <ernvint@gmail.com>
2017-02-05 15:31:42 +0300

    smth

--- hbsuck/ui/js/i18n/en.js
+++
@@ -1,9 +0,0 @@
-var i18n = {
-  'Close': 'Close',
-  'categories' : 'categories',
-  'Enter email address': 'Enter email address',
-  'Thank you, an email will be on its way.': 'Thank you, an email will be on its way.',
-  'Browse the store': 'Browse the store',
-  'Hide menu': 'Hide menu',
-  'Basket loading': 'Basket loading'
-};
--- hbsuck/ui/js/libs/README.md
+++
@@ -1,5 +0,0 @@
-# Javascript Libraries
-
-All Javascript libraries should go in here. Specifically, the folder should (try) and match the namespace.
-
-All libraries that don't fit into the project names space should go in the root folder.
